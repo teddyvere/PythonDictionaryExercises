c8ae5375054168c1dddd15cda2ad910864673854
ErenBtrk <erenbtrk@hotmail.com>
2021-05-16 16:05:50 +0300

    Exercise 6

---
+++ Exercise6.py
@@ -0,0 +1,16 @@
+'''
+6. Write a Python script to generate and print a dictionary that contains a number
+ (between 1 and n) in the form (x, x*x).
+Sample Dictionary ( n = 5) :
+Expected Output : {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+'''
+
+dict1 = {}
+
+n = int(input("Please enter a number : "))
+
+for index in range(1,n+1):
+    dict1[index] = index**2
+
+print(dict1)
