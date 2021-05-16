eb57ed6bcb0a4f2060c06e8c0a8fa083b4d26098
ErenBtrk <erenbtrk@hotmail.com>
2021-05-16 18:27:14 +0300

    Exercise 18

---
+++ Exercise18.py
@@ -0,0 +1,10 @@
+'''
+18. Write a Python program to check a dictionary is empty or not.
+'''
+dict1 = {}
+dict2 = {1:5}
+
+if(not dict1):
+    print("The dictionary is empty.")
+else:
+    print("The dictionary is not empty.")
