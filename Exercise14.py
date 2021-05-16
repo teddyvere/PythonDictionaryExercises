2632bb1be5d8f332319ebea24d7072a7e4e01f7a
ErenBtrk <erenbtrk@hotmail.com>
2021-05-16 18:11:30 +0300

    Exercise 14

---
+++ Exercise14.py
@@ -0,0 +1,23 @@
+'''
+14. Write a Python program to sort a dictionary by key.
+
+'''
+import operator
+
+d = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+print('Original dictionary : ',d)
+sorted_d = sorted(d.items(), key=operator.itemgetter(0))
+print('Dictionary in ascending order by value : ',sorted_d)
+sorted_d = dict( sorted(d.items(), key=operator.itemgetter(0),reverse=True))
+print('Dictionary in descending order by value : ',sorted_d)
+
+
+########################################################################################################################
+
+color_dict = {'red':'#FF0000',
+          'green':'#008000',
+          'black':'#000000',
+          'white':'#FFFFFF'}
+
+for key in sorted(color_dict):
+    print("%s: %s" % (key, color_dict[key]))
